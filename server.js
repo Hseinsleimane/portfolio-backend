b6bd90e59eea7e5b0114470152486981c5b6a387
houssein <82110097@students.liu.edu.lb>
2024-07-20 12:57:58 +0300

    model,controllers,routes and setup for backend ready

---
+++ server.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const connection = require('./database/connection');
+const express=require('express');
+const app=express();
+const cors=require('cors');
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+ const userRoutes = require('./routes/users');
+// const terrainRoutes = require('./routes/terrainRoute');
+// const storeRoutes = require('./routes/storeRoute');
+// const cafeteriaRoutes = require('./routes/cafeteriaRoute');
+// const eventsRoutes = require('./routes/eventRoute');
+// const bookingsRoutes = require('./routes/bookingRoute');
+// const mailRoutes = require('./routes/mailRoute');
+ 
+ app.use('/users', userRoutes);
+// app.use('/terrains', terrainRoutes);
+// app.use('/store', storeRoutes);
+// app.use('/cafeteria', cafeteriaRoutes);
+// app.use('/events', eventsRoutes);
+// app.use('/bookings', bookingsRoutes);
+// app.use('/mail', mailRoutes);
+
+
+app.listen(process.env.PORT,()=>{
+    connection.checkConnection();
+    console.log(`server is running on port:${process.env.PORT}`)
+})
