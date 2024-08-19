8aa9e6f9c5c2bcebb3fd13efa4bfbf4651d06c1f
yaramounzr <monzeryara@gmail.com>
2024-08-19 13:32:01 +0300

    adjusted

--- frontend/src/components/Certificates.js
+++ frontend/src/components/Certificates.js
@@ -6,14 +6,14 @@ const Certificates = ({ certificatesData }) => {
   return (
     <section className="bg-[var(--box-background)] text-[var(--text-color-heading)] py-20" id="certificates">
       <div className="section-container">
-      <motion.h2
-    className="text-5xl font-bold text-center mb-12 text-[var(--text-color-heading)]"
-    initial={{ opacity: 0, y: -50 }}
-    animate={{ opacity: 1, y: 0 }}
-    transition={{ duration: 0.5 }}
-  >
-    Certificates
-  </motion.h2>
+        <motion.h2
+          className="text-5xl font-bold text-center mb-12 text-[var(--text-color-heading)]"
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          Certificates
+        </motion.h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {certificatesData.map((certificate, index) => (
             <motion.div
--- frontend/src/components/FlipCard.js
+++ frontend/src/components/FlipCard.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
-import CircularProgress from './CircularProgress';
 
 const FlipCard = ({ certificate }) => {
   const [isFlipped, setIsFlipped] = useState(false);
@@ -14,13 +13,12 @@ const FlipCard = ({ certificate }) => {
       onClick={() => setIsFlipped(!isFlipped)}
     >
       <div className="flip-card-inner w-full h-full relative">
-        <div className="flip-card-front absolute w-full h-full bg-[var(--box-background)] p-6 rounded-lg shadow-lg flex flex-col justify-between">
-          <h3 className="text-2xl font-bold">{certificate.title}</h3>
-          <p className="text-lg">{certificate.issuer}</p>
-          <p className="text-sm">{new Date(certificate.date).toLocaleDateString()}</p>
-          <CircularProgress percentage={certificate.proficiency || 100} />
+        <div className="flip-card-front absolute w-full h-full bg-[var(--box-background)] p-4 rounded-lg shadow-lg flex flex-col justify-between">
+          <h3 className="text-2xl font-bold mb-2">{certificate.title}</h3>
+          <p className="text-lg mb-1">{certificate.issuer}</p>
+          <p className="text-sm mb-1">{new Date(certificate.date).toLocaleDateString()}</p>
         </div>
-        <div className="flip-card-back absolute w-full h-full bg-[var(--accent-color)] text-white p-6 rounded-lg shadow-lg flex flex-col justify-center items-center transform rotateY-180">
+        <div className="flip-card-back absolute w-full h-full bg-[var(--accent-color)] text-white p-4 rounded-lg shadow-lg flex flex-col justify-center items-center transform rotateY-180">
           <p className="text-center">{certificate.description}</p>
         </div>
       </div>
